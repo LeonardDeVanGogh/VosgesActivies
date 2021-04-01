b429cd8262a292220aed84d2b9e2f26116baf223
Bryan Razyk <bryan.razyk@gmail.com>
2021-04-01 16:28:09 +0200

    add an interface to update users roles

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\EditUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
 
@@ -30,5 +33,22 @@ class AdminController extends AbstractController
     	return $this->render('admin/users.html.twig', [
     		'users' => $users->findAll()
     	]);
+    }
+    /**
+     * @Route("/user/update/{id}", name="user_role_update")
+     */
+    public function userRoleUpdate(User $user, Request $request){
+        $form = $this->createForm(EditUserType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('message', 'Utilisateur modifié avec succès');
+            return $this->redirectToRoute('admin_users');
+        }
+        return $this->render('admin/edit_user.html.twig', [
+            'userForm' => $form->createView(),
+        ]);
     }
 }
