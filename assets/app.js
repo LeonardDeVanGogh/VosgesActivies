cd02d7cb210b067726f30086f35852abd89f552d
Bryan Razyk <bryan.razyk@gmail.com>
2021-04-02 17:46:35 +0200

    update view of navbar

--- assets/app.js
+++ assets/app.js
@@ -13,8 +13,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
-import $ from 'jquery';
+
 
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+ import $ from 'jquery';
